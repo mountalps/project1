535ef32fa25441576c285ea3c37cadb3c33b76ee
Wei <wesleyoneway@hotmail.com>
2018-05-06 12:05:17 -0400

    publish a job complete
    publish a job complete

--- lib/fun.php
+++ lib/fun.php
@@ -54,3 +54,6 @@ function checkLongin(){
 
 
 
+
+
+
--- zq/company_publish_jobs_result.php
+++ zq/company_publish_jobs_result.php
@@ -13,6 +13,7 @@
     $sqlGetCompanyInfo = "select * from Company where cusername = '{$username}';";
     $resultCompanyInfo = mysqli_query($conToDB, $sqlGetCompanyInfo);
     $companyInfo = mysqli_fetch_all($resultCompanyInfo, MYSQLI_ASSOC);
+//    var_dump($companyInfo);
     
     
     //Job Table Attributes:
@@ -45,16 +46,75 @@
 //    var_dump($job_major);
 //    var_dump($job_description);
     
+//    if ($job_title == "" || $job_city == "" || $job_state == "" || $job_country == "" || $job_salary == "" || $job_degree == "" || $job_major == "" || $job_description == ""){
+//        if ($job_title == "")
+//            echo 'job title cannot be empty!<\br>';
+//        if ($job_city == "")
+//            echo 'job city cannot be empty!<\br>';
+//        if ($job_state == "")
+//            echo 'job state cannot be empty!<\br>';
+//        if ($job_country == "")
+//            echo 'job country cannot be empty!<\br>';
+//        if ($job_salary== "")
+//            echo 'job salary cannot be empty!<\br>';
+//        if ($job_degree== "")
+//            echo 'job degree cannot be empty!<\br>';
+//        if ($job_major == "")
+//            echo 'job major cannot be empty!<\br>';
+//        if ($job_description == "")
+//            echo 'job description cannot be empty!<\br>';
+//    }
     
+    $job_salary = (int)$job_salary;
+//    var_dump($job_salary);
+    //          `jid` INT NOT NULL auto_increment,
+    //  `title` VARCHAR(100) NOT NULL,
+    //  `cid` INT NOT NULL,
+    //  `jcity` VARCHAR(20) NOT NULL,
+    //  `jstate` VARCHAR(20) NOT NULL,
+    //  `jcountry` VARCHAR(20) NOT NULL,
+    //  `salary` MEDIUMINT NOT NULL,
+    //  `degree` VARCHAR(20) NOT NULL,
+    //  `major` VARCHAR(20) NOT NULL,
+    //  `jdescription` TEXT NOT NULL,
+    
+    $sqlPublishAJob = "insert into Job values
+(null, '{$job_title}', '{$companyInfo[0]['cid']}', '{$job_city}', '{$job_state}', '{$job_country}', '{$job_salary}', '{$job_degree}', '{$job_major}', '{$job_description}');";
+    $resultPublishAJob = mysqli_query($conToDB, $sqlPublishAJob);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Publish a new job</title>
 </head>
 <body>
+<div class="navivation">
+    <nav>
+        <div class="wrapper">
+            <a class="active" href="0_company-homepage.php">Home</a> |
+            <a href="company_notifications.php">Notifications</a> |
+            <a href="company_jobs.php">Your Jobs</a> |
+            <a href="company_publish_jobs.php">Publish A Job</a> |
+            <a href="company_modify_profile.php">Modify Profile</a> |
+            <form action="company_search_result.php" method="get" id="keyword_search">
+                <input type="text" placeholder="Search..." name="keyword">
+                <button type="submit">search</button>
+            </form>
+        </div>
+    </nav>
+</div>
+
+<?php if ($resultPublishAJob):?>
+    <h2>Job Created Successfully!</h2>
+    
+<?php else:?>
+    <h2>Job Created Unsuccessfully!</h2>
+    <h2>Please Try Again!</h2>
+    <button onclick="window.location.href='company_publish_jobs.php'">Try Again</button>
+
+<?php endif;?>
 
 </body>
 </html>
