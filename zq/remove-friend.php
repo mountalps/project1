8859e64b6d8cfd68f8597b441037a0707d43f341
zhenfengqi <dennis09121111@gmail.com>
2018-05-11 13:04:22 -0400

    remove friend

--- zq/remove-friend.php
+++ zq/remove-friend.php
@@ -22,10 +22,12 @@ if ($checkUser != "student"){
             }
             $sid1 = $_POST['sid1'];
             $sid2 = $_POST['sid2'];
+            var_dump($sid1);
+            var_dump($sid2);
             if ((int)$sid1 > (int)$sid2) {
                 $conn->query("delete from Friend where sid1 = {$sid2} and sid2 = {$sid1};");
                 $conn->query("delete from NotificationToStudent where (fromsid = {$sid1} and tosid = {$sid2}) or (fromsid = {$sid2} and tosid = {$sid1});");
-            } else if ((int)$sid1 > (int)$sid2) {
+            } else if ((int)$sid1 < (int)$sid2) {
                 $conn->query("delete from Friend where sid1 = {$sid1} and sid2 = {$sid2};");
                 $conn->query("delete from NotificationToStudent where (fromsid = {$sid1} and tosid = {$sid2}) or (fromsid = {$sid2} and tosid = {$sid1});");
             }
