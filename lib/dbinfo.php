28bf5e9429fbb6c65f8721f8a2e40477a5be4cb1
Wei <wesleyoneway@hotmail.com>
2018-05-29 09:56:41 -0400

    delete dbinfo
    delete dbinfo

--- lib/dbinfo.php
+++ lib/dbinfo.php
@@ -7,10 +7,6 @@
  */
 
 
-$DBhost = 'dbprojectjobster.cinhv01qdhxv.us-east-2.rds.amazonaws.com';
-$DBuser = 'mountalps';
-$DBpassword = 'pt4-ovc-LMe-sVL';
-$DBdatabase = 'dbproject_new';
-$port = '3306';
+
 
 ?>
